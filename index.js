075bea6d33ba96d24a36917fb14460a5497c862c
Bale admin <bale_admin@elenoon.ir>
2018-07-02 16:50:10 +0430

    Renamed old directories

--- index.js
+++ index.js
@@ -2,42 +2,42 @@
  * Created by amin on 9/6/16.
  */
 
-exports.BaleBot = require("./lib/ir/elenoon/botplatform/layers/implementation/BaleBot").BaleBot;
+exports.BaleBot = require("./lib/ir/bale/botplatform/layers/implementation/BaleBot").BaleBot;
 
 /**Message models*/
-exports.TextMessage = require("./lib/ir/elenoon/botplatform/models/message/TextMessage").TextMessage;
-exports.FileMessage = require("./lib/ir/elenoon/botplatform/models/message/FileMessage").FileMessage;
-exports.PhotoMessage = require("./lib/ir/elenoon/botplatform/models/message/PhotoMessage").PhotoMessage;
-exports.VideoMessage = require("./lib/ir/elenoon/botplatform/models/message/VideoMessage").VideoMessage;
-exports.AudioMessage = require("./lib/ir/elenoon/botplatform/models/message/AudioMessage").AudioMessage;
-exports.StickerMessage = require("./lib/ir/elenoon/botplatform/models/message/StickerMessage").StickerMessage;
-exports.QuotedMessage = require("./lib/ir/elenoon/botplatform/models/message/QuotedMessage").QuotedMessage;
-exports.ReceiptMessage = require("./lib/ir/elenoon/botplatform/models/message/ReceiptMessage").ReceiptMessage;
+exports.TextMessage = require("./lib/ir/bale/botplatform/models/message/TextMessage").TextMessage;
+exports.FileMessage = require("./lib/ir/bale/botplatform/models/message/FileMessage").FileMessage;
+exports.PhotoMessage = require("./lib/ir/bale/botplatform/models/message/PhotoMessage").PhotoMessage;
+exports.VideoMessage = require("./lib/ir/bale/botplatform/models/message/VideoMessage").VideoMessage;
+exports.AudioMessage = require("./lib/ir/bale/botplatform/models/message/AudioMessage").AudioMessage;
+exports.StickerMessage = require("./lib/ir/bale/botplatform/models/message/StickerMessage").StickerMessage;
+exports.QuotedMessage = require("./lib/ir/bale/botplatform/models/message/QuotedMessage").QuotedMessage;
+exports.ReceiptMessage = require("./lib/ir/bale/botplatform/models/message/ReceiptMessage").ReceiptMessage;
 // template messages
-exports.TemplateMessage = require("./lib/ir/elenoon/botplatform/models/message/template/TemplateMessage").TemplateMessage;
-exports.SimpleTemplate = require("./lib/ir/elenoon/botplatform/models/message/template/SimpleTemplate").SimpleTemplate;
-exports.ButtonElement = require("./lib/ir/elenoon/botplatform/models/message/template/ButtonElement").ButtonElement;
-exports.PurchaseMessage = require("./lib/ir/elenoon/botplatform/models/message/PurchaseMessage").PurchaseMessage;
-exports.User = require("./lib/ir/elenoon/botplatform/models/User").User;
-exports.Group = require("./lib/ir/elenoon/botplatform/models/Group").Group;
-exports.MoneyRequestType = require("./lib/ir/elenoon/botplatform/models/MoneyRequestType").MoneyRequestType;
+exports.TemplateMessage = require("./lib/ir/bale/botplatform/models/message/template/TemplateMessage").TemplateMessage;
+exports.SimpleTemplate = require("./lib/ir/bale/botplatform/models/message/template/SimpleTemplate").SimpleTemplate;
+exports.ButtonElement = require("./lib/ir/bale/botplatform/models/message/template/ButtonElement").ButtonElement;
+exports.PurchaseMessage = require("./lib/ir/bale/botplatform/models/message/PurchaseMessage").PurchaseMessage;
+exports.User = require("./lib/ir/bale/botplatform/models/User").User;
+exports.Group = require("./lib/ir/bale/botplatform/models/Group").Group;
+exports.MoneyRequestType = require("./lib/ir/bale/botplatform/models/MoneyRequestType").MoneyRequestType;
 
 /** Conversation models */
-exports.Conversation = require("./lib/ir/elenoon/botplatform/utils/Conversation").Conversation;
+exports.Conversation = require("./lib/ir/bale/botplatform/utils/Conversation").Conversation;
 
 /** Sensitives **/
-exports.PhotoMessageSensitive = require("./lib/ir/elenoon/botplatform/utils/sensitive/PhotoMessageSensitive").PhotoMessageSensitive;
-exports.FileMessageSensitive = require("./lib/ir/elenoon/botplatform/utils/sensitive/FileMessageSensitive").FileMessageSensitive;
-exports.TextMessageSensitive = require("./lib/ir/elenoon/botplatform/utils/sensitive/TextMessageSensitive").TextMessageSensitive;
+exports.PhotoMessageSensitive = require("./lib/ir/bale/botplatform/utils/sensitive/PhotoMessageSensitive").PhotoMessageSensitive;
+exports.FileMessageSensitive = require("./lib/ir/bale/botplatform/utils/sensitive/FileMessageSensitive").FileMessageSensitive;
+exports.TextMessageSensitive = require("./lib/ir/bale/botplatform/utils/sensitive/TextMessageSensitive").TextMessageSensitive;
 
 /** Requests **/
-exports.SendMessageRequest = require("./lib/ir/elenoon/botplatform/models/clientMessages/SendMessageRequest").SendMessageRequest;
-exports.GetKeysRequest = require("./lib/ir/elenoon/botplatform/models/clientMessages/GetKeysRequest").GetKeysRequest;
-exports.SetValueRequest = require("./lib/ir/elenoon/botplatform/models/clientMessages/SetValueRequest").SetValueRequest;
-exports.GetValueRequest = require("./lib/ir/elenoon/botplatform/models/clientMessages/GetValueRequest").GetValueRequest;
-exports.DeleteValueRequest = require("./lib/ir/elenoon/botplatform/models/clientMessages/DeleteValueRequest").DeleteValueRequest;
+exports.SendMessageRequest = require("./lib/ir/bale/botplatform/models/clientMessages/SendMessageRequest").SendMessageRequest;
+exports.GetKeysRequest = require("./lib/ir/bale/botplatform/models/clientMessages/GetKeysRequest").GetKeysRequest;
+exports.SetValueRequest = require("./lib/ir/bale/botplatform/models/clientMessages/SetValueRequest").SetValueRequest;
+exports.GetValueRequest = require("./lib/ir/bale/botplatform/models/clientMessages/GetValueRequest").GetValueRequest;
+exports.DeleteValueRequest = require("./lib/ir/bale/botplatform/models/clientMessages/DeleteValueRequest").DeleteValueRequest;
 
 
 /** Other */
-exports.SDKConst = require("./lib/ir/elenoon/botplatform/constants/SDKConst").SDKConst;
-exports.Logger = require("./lib/ir/elenoon/botplatform/utils/Logger").Logger;
+exports.SDKConst = require("./lib/ir/bale/botplatform/constants/SDKConst").SDKConst;
+exports.Logger = require("./lib/ir/bale/botplatform/utils/Logger").Logger;
